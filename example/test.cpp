25c4ca719607914e9c6f7dc5901875b4e81231dd
Brian T <btsan@ucmerced.edu>
2019-10-14 00:02:08 -0700

    updated example code
    Now saves server results into memory before printing into stdout

--- example/test.cpp
+++ example/test.cpp
@@ -1,7 +1,31 @@
-
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <curl/curl.h>
+
+struct MemoryStruct {
+	char *memory;
+	size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	size_t realsize = size * nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+	char *ptr = (char *)realloc(mem->memory, mem->size + realsize + 1);
+	if (ptr == NULL) {
+		/* out of memory */
+		std::cerr << "Not enough memory (realloc returned NULL)\n";
+		return 0;
+	}
+
+	mem->memory = ptr;
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
+
+	return realsize;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -20,14 +44,24 @@ int main(int argc, char const *argv[])
 		json_file.close();
 	}
 
+	struct MemoryStruct chunk;
+	chunk.memory = (char *)malloc(1);
+	chunk.size = 0;
+
 	curl = curl_easy_init();
 	if (curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8501/v1/models/TinyModel:predict");
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK) {
 			std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+		}
+		else {
+			std::cout << (unsigned long)chunk.size << " bytes retrieved\n";
+			printf("Here is the JSON results:\n%s", chunk.memory);
 		}
 		curl_easy_cleanup(curl);
 	}
