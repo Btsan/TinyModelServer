bb5d3a08eea241757d9f05a52835dc01bb58b2a7
Brian T <btsan@ucmerced.edu>
2019-10-14 00:19:04 -0700

    commented test.cpp

--- example/test.cpp
+++ example/test.cpp
@@ -3,14 +3,22 @@
 #include <fstream>
 #include <curl/curl.h>
 
+// This struct will store the server results
 struct MemoryStruct {
 	char *memory;
 	size_t size;
 };
 
-static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+// This function gets called whenever data is received and logically updates the buffer
+// 		contents points at the received data
+// 		size is always 1 (according to docs)
+// 		nmemb is the size of the contents
+// 		userdata points to the struct that the contents can be written to
+//
+//	This example uses C style memory allocation, but can be implemented with C++ vectors too
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userdata) {
 	size_t realsize = size * nmemb;
-	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userdata;
 
 	char *ptr = (char *)realloc(mem->memory, mem->size + realsize + 1);
 	if (ptr == NULL) {
@@ -19,6 +27,7 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 		return 0;
 	}
 
+	// C style buffer update
 	mem->memory = ptr;
 	memcpy(&(mem->memory[mem->size]), contents, realsize);
 	mem->size += realsize;
@@ -31,6 +40,8 @@ int main(int argc, char const *argv[])
 {
 	CURL *curl;
 	CURLcode res;
+
+	// test data is read into json_data
 	char *json_data;
 	std::ifstream json_file;
 	json_file.open("test.json");
@@ -44,6 +55,7 @@ int main(int argc, char const *argv[])
 		json_file.close();
 	}
 
+	// C style initialization of a memory buffer
 	struct MemoryStruct chunk;
 	chunk.memory = (char *)malloc(1);
 	chunk.size = 0;
@@ -53,13 +65,15 @@ int main(int argc, char const *argv[])
 		curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8501/v1/models/TinyModel:predict");
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk); // this is how chunk gets passed to our callback
 
+		// send the request!
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK) {
 			std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
 		}
 		else {
+			/* Do something with the results here */
 			std::cout << (unsigned long)chunk.size << " bytes retrieved\n";
 			printf("Here is the JSON results:\n%s", chunk.memory);
 		}
